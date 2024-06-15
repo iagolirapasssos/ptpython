72a6f2a8ec6c8f3506922dc8626731a1c5c1112e
iagolirapassos <iagolirapassos@gmail.com>
2024-06-15 15:13:49 -0300

    Bump version to 0.1.135

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ptpython",
-    version='0.1.134',
+    version='0.1.135',
     packages=find_packages(),
     install_requires=[
         # Dependências aqui (por exemplo: 'ply==3.11' se precisar de um lexer/parser)
