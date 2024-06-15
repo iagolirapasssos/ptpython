126c69a584db89e6b9e4a28311418c9e0c567565
iagolirapassos <iagolirapassos@gmail.com>
2024-06-15 16:19:03 -0300

    Ajustes na documentação do Sphinx

--- docs/conf.py
+++ docs/conf.py
@@ -1,14 +1,12 @@
 # Configuration file for the Sphinx documentation builder.
 
 # -- Project information -----------------------------------------------------
-
 project = 'ptpython'
 author = 'Francisco Iago Lira Passos'
 release = '0.1'
-version = '0.1.0'
+version = '0.1.176'
 
 # -- General configuration ---------------------------------------------------
-
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.napoleon',
@@ -19,18 +17,10 @@ templates_path = ['_templates']
 exclude_patterns = []
 
 # -- Options for HTML output -------------------------------------------------
-
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-# -- Options for LaTeX output ------------------------------------------------
-
-latex_elements = {
-    'preamble': '',
-}
-
 # -- Extension configuration -------------------------------------------------
-
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
---
+++ docs/index.rst
@@ -0,0 +1,18 @@
+.. ptpython documentation master file, created by
+   sphinx-quickstart on Tue Oct 19 12:14:29 2021.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to ptpython's documentation!
+====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
