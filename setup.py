a9189440f24f887dde46d5ff1a8843005633df26
iagolirapassos <iagolirapassos@gmail.com>
2024-06-15 15:15:25 -0300

    Bump version to 0.1.171

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ptpython",
-    version='0.1.170',
+    version='0.1.171',
     packages=find_packages(),
     install_requires=[
         # Dependências aqui (por exemplo: 'ply==3.11' se precisar de um lexer/parser)
