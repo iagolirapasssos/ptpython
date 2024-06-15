b4a97794e0192e38c0682bf7b4339a62f24ce8af
iagolirapassos <iagolirapassos@gmail.com>
2024-06-15 15:12:29 -0300

    Bump version to 0.1.98

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ptpython",
-    version='0.1.97',
+    version='0.1.98',
     packages=find_packages(),
     install_requires=[
         # Dependências aqui (por exemplo: 'ply==3.11' se precisar de um lexer/parser)
