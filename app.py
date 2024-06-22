7b45af121cdd842953897b82efe49bb662a96979
iagolirapassos <iagolirapassos@gmail.com>
2024-06-22 15:20:45 -0300

    API added

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 import os
 from tempfile import NamedTemporaryFile
 import subprocess
 from ptpython.translate import translate
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def index():
@@ -42,4 +44,3 @@ def run_code():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
