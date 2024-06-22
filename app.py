e06695365d7f0146909c771d104fc273cc6f59a4
iagolirapassos <iagolirapassos@gmail.com>
2024-06-22 16:35:33 -0300

    Correções na API

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ import threading
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
-socketio = SocketIO(app, cors_allowed_origins="*")
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
