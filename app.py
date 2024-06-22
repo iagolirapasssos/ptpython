89e115a664e81c32fafdcc918390954b6624244f
iagolirapassos <iagolirapassos@gmail.com>
2024-06-22 20:31:10 -0300

    Correções na API

--- app.py
+++ app.py
@@ -1,10 +1,9 @@
-from ptpython.translate import translate
 from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 import os
 from tempfile import NamedTemporaryFile
 import subprocess
-import re
+from ptpython.translate import translate
 
 app = Flask(__name__)
 CORS(app, resources={r"/api2/*": {"origins": "*"}})
@@ -29,10 +28,10 @@ def translate_code():
 def run_code():
     data = request.json
     code = data.get('code', '')
-    translated_code = translate(code)
-
-    input_prompts = extract_input_prompts(translated_code)
     user_inputs = data.get('inputs', {})
+
+    translated_code = translate(code)
+    input_prompts = extract_input_prompts(translated_code)
 
     with NamedTemporaryFile(mode='w+', suffix='.py', delete=False) as temp_file:
         temp_file.write(translated_code)
@@ -44,6 +43,7 @@ def run_code():
     return jsonify({'output': output, 'prompts': input_prompts})
 
 def extract_input_prompts(code):
+    import re
     pattern = r'input\("([^"]+)"\)'
     return re.findall(pattern, code)
 
@@ -56,8 +56,13 @@ def execute_code(temp_filename, user_inputs):
         text=True
     )
 
-    input_responses = "\n".join(user_inputs.get(prompt, '') for prompt in user_inputs) + "\n"
-    output, error = process.communicate(input=input_responses)
+    def get_input(prompt):
+        return user_inputs.get(prompt, '') + '\n'
+
+    inputs = [get_input(prompt) for prompt in extract_input_prompts(open(temp_filename).read())]
+    input_data = ''.join(inputs)
+
+    output, error = process.communicate(input=input_data)
     return output + error
 
 if __name__ == '__main__':
