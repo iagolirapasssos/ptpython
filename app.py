ba12f26adf45734238421b9ec0aa617cc1ea1a87
iagolirapassos <iagolirapassos@gmail.com>
2024-06-24 15:59:07 -0300

    Correções na API com websocket

--- app.py
+++ app.py
@@ -2,19 +2,14 @@ from flask import Flask, request, jsonify, Response
 from flask_cors import CORS
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
-from flask_socketio import SocketIO, emit
 import os
 from tempfile import NamedTemporaryFile
 import subprocess
 from ptpython.translate import translate
 import redis
-import base64
-import matplotlib.pyplot as plt
-import io
 
 app = Flask(__name__)
 CORS(app, resources={r"/api2/*": {"origins": "*"}})
-socketio = SocketIO(app)
 
 # Configuração do Redis para Flask-Limiter
 redis_client = redis.Redis(host='localhost', port=6379, db=0)
@@ -65,7 +60,6 @@ def run_code():
 
     result_output = output.strip() + '\n' + error.strip()
     
-    print({'output': result_output, 'prompts': input_prompts})
     return jsonify({'output': result_output, 'prompts': input_prompts})
 
 def inputs_only(user_inputs):
@@ -106,7 +100,6 @@ def execute_code(temp_filename, user_inputs):
     # Remove os prompts de entrada da saída
     filtered_output = filter_output(output, input_prompts)
 
-    print(f"\n\noutput: {filtered_output}, error: {error}, {filtered_output + error}\n\n")
     return filtered_output, error
 
 def filter_output(output, prompts):
@@ -114,36 +107,5 @@ def filter_output(output, prompts):
         output = output.replace(prompt, '')
     return output
 
-@socketio.on('execute_code')
-def handle_execute_code(data):
-    code = data['code']
-    user_inputs = data.get('inputs', {})
-
-    translated_code = translate(code)
-
-    if contains_dangerous_commands(translated_code):
-        emit('code_output', {'output': 'Código contém comandos potencialmente perigosos.'})
-        return
-
-    input_prompts = extract_input_prompts(translated_code)
-
-    with NamedTemporaryFile(mode='w+', suffix='.py', delete=False) as temp_file:
-        temp_file.write(translated_code)
-        temp_file.flush()
-        temp_filename = temp_file.name
-
-    output, error = execute_code(temp_filename, user_inputs)
-    os.remove(temp_filename)
-
-    result_output = output.strip() + '\n' + error.strip()
-
-    # Se o código gerar um gráfico, enviar o gráfico para o frontend
-    if 'plt.show()' in translated_code:
-        with open('exemplo_grafico.png', 'rb') as image_file:
-            encoded_string = base64.b64encode(image_file.read()).decode('utf-8')
-        emit('graph_output', {'image_data': encoded_string})
-    
-    emit('code_output', {'output': result_output, 'prompts': input_prompts})
-
 if __name__ == '__main__':
-    socketio.run(app, debug=True, host='0.0.0.0', port=6000)
+    app.run(debug=True, host='0.0.0.0', port=6000)
